a3feb0938653976c8874ab3c7b15b47e40241a1d
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-07-02 18:16:23 +0300

    Basic identification finished

--- src/AccountInformation.js
+++ src/AccountInformation.js
@@ -4,14 +4,14 @@ import {Line} from './Form'
 
 const AccInfoWrapp = styled.div`
 margin-top: 20px;
-display:${props=> props.visible ? 'block' : 'none'}
+display:${props=> props.visible ? 'block' : 'none'};
 
 `
-
+const AdressBook = styled.p`
+margin-top:20px;
+`
 export const AccountInformation = ({info, visible}) => {
     
-
-
     return (
         <AccInfoWrapp visible={visible}>
             <h3>Account Information</h3>
@@ -22,6 +22,12 @@ export const AccountInformation = ({info, visible}) => {
             <p> <b>Newsletter:</b>{info.checkbox ?
                 "You've subscribed to our newsletter" :
                 "You aren't subscribed to our newsletter"}</p>
+            
+            <AdressBook><b>Adress Book</b></AdressBook>
+            <Line />
+            <p>{info.street}</p>
+            <p>{info.country}</p>
+            <p>{info.region}</p>
         </AccInfoWrapp>
     )
 }
